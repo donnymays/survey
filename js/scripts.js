5faea06c3e366fa3f4d300648009e91f5dca472e
donnymays <dtmays2@gmail.com>
2020-08-12 16:16:42 -0700

    create survey skeleton
    co-authored-by: Natalie Murphy <nataliemurphy500@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    const name = $("input#name").val();
+    const dob = $("#born").val();
+    const beverage = $("#beverage").val();
+    const flavor = $("input:radio[name=flavor]:checked").val();
+    const favoriteColor = $("#color").val();
+  )};
+)};
